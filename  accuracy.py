214247b253fde1f610f6f5e5b3749cffa5bb7040
vaishnaviaids24-ship-it <vaishnavi.aids24@cmrit.ac.in>
2025-12-08 23:26:04 +0530

    commit -m"Latest changes"

---
+++  accuracy.py
@@ -0,0 +1,21 @@
+import tensorflow as tf
+from tensorflow.keras import backend as K
+
+def recall_m(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    return true_positives / (possible_positives + K.epsilon())
+
+def precision_m(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    return true_positives / (predicted_positives + K.epsilon())
+
+def f1_m(y_true, y_pred):
+    precision = precision_m(y_true, y_pred)
+    recall = recall_m(y_true, y_pred)
+    return 2*((precision*recall)/(precision+recall+K.epsilon()))
+
+model.compile(optimizer='adam',
+              loss='binary_crossentropy',
+              metrics=['accuracy', f1_m, precision_m, recall_m])
